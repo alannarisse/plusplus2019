eeaf61a9e5185765ac9df787b91ece1cae728517
alannarisse <angrypirate@gmail.com>
2019-04-23 12:58:39 -0700

    column changes and images to ideas

--- wp-content/themes/plusplus2019/home.php
+++ wp-content/themes/plusplus2019/home.php
@@ -5,23 +5,25 @@
  -->
 		<?php 
 		$args = array(
-        'posts_per_page' => 1,
-        'post__in' => get_option( 'sticky_posts' ),
-        'ignore_sticky_posts' => 1);
+        	'posts_per_page' => 2,
+        	'post__in' => get_option( 'sticky_posts' ),
+        	'ignore_sticky_posts' => 2);
 		$sticky_query = new WP_Query( $args );
 		?>
 		<?php if(have_posts()) : ?>
 			<?php while($sticky_query->have_posts()) : $sticky_query->the_post(); ?>
 			<div>
-				<strong>Featured Post</strong>
 				<?php get_template_part('content'); ?>
 			</div>
 			<?php endwhile; ?>
 		<?php endif; ?>
 		</section>
 		<section>
+			<?php
+				$query = new WP_Query( array( 'post__not_in' => get_option( 'sticky_posts' ) ) ); ?>
 			<?php if(have_posts()) : ?>
-			<?php while(have_posts()) : the_post(); ?>
+				
+			<?php while($query->have_posts()) : $query->the_post(); ?>
 				<?php get_template_part('content'); ?>
 			<?php endwhile; ?>
 			<?php else : ?>
@@ -31,9 +33,9 @@
 		<section>
 			
 			<div style="height:120px;">&nbsp;</div>
-			<div>
-				<img src="images/horiz_single_pluses.svg" style="width: 246px"></div>
-			<div><img src="images/bkg_horiz.svg" style="margin-left: -120px;"></div>
+			<div style="width: 246px">
+				<?php get_template_part('images/inline', 'bhoriz_single_pluses.svg'); ?></div>
+			<div style="margin-left: -120px;"><?php get_template_part('images/inline', 'bkg_horiz.svg'); ?></div>
 
 
 		</section>
