146c0fd4ff49a19d6d97db64ddc6d57c6f3d797e
alannarisse <angrypirate@gmail.com>
2019-04-26 11:22:13 -0700

    adding products

---
+++ wp-content/themes/plusplus2019/page-app.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+
+	<main>
+		<section>
+		<div>
+
+			<h2>Enterprise knowledge sharing platform</h2>
+		</div>
+		<div>
+			<span class="round-bdr"><img class="round" src="/wp-content/themes/plusplus2019/images/photos/happy_business_people.jpeg" style=""></span>
+			<h4>For your people, PlusPlus makes it easy to discover workshops, classes, tech talks, and hackathons. For the Learning & Development team, it makes it easy to manage learning at scale.</h4>
+		</div>
+
+
+		<div class="horiz-left" style="margin-bottom:4em;margin-left:-200px;"><?php get_template_part('images/inline', 'bkg_horiz.svg'); ?></div><div>&nbsp;</div>
+
+		<div>
+			<img src="/wp-content/themes/plusplus2019/images/products/Coaching-index.png" style="width: 100%;">
+		</div>
+		<div>
+<h3>Coaching and Mentoring</h3>
+<p>Effective knowledge transfer at scale that’s as unique as your organization? Tailored to each employee’s needs? Yes!</p>
+
+<p>With PlusPlus you can instill a culture of learning that puts your workforce first. Experts schedule time for onboarding and training. Those seeking new skills or advice connect to experts they need. Voila! The effectiveness of tribal knowledge transfer increased without an impact on productivity.</p>
+<!-- 
+No fruitless searching for the right knowledge.
+Only 38% of employees feel they have access to long-term development at work. With the PlusPlus Coaching and Mentoring module, employees control their own career development. Now everyone has unique learning paths. Choose coaching (program-first discovery), mentoring (people-first discovery), or instructor-led training (ILT).
+
+No need to know who to approach or when.
+6 out of 10 employees want to learn at the speed of need. The PlusPlus Coaching and Mentoring module makes this happen.
+
+Find-Top-Mentors-Available-Mentorship-Programs
+
+Experts are only a screen away. Newbies can find the available expert they need. PlusPlus handles the actual virtual introduction and all calendaring.
+
+Speed interactions by including coaching or mentoring in an onboarding track.
+
+A data-based discovery of your organization’s most effective Guides.
+80% of workforce learning happens through on-the-job interactions with peers, teammates, and managers. PlusPlus, unlike other offerings available, focuses on this 1:1 tribal knowledge transfer.
+
+The PlusPlus Coaching and Mentoring module highlights your best coaches and mentors. (Together coaches, mentors, trainers, and presenters are Guides.) Your workforce can now identify who to learn from. People teams know the most in-demand and most effective coaches and mentors. L&D can help overextended mentors create ILT to reach more people with less effort.
+ -->
+
+		</div>
+
+	</section>
+	</main>
+	<?php get_footer(); ?>
