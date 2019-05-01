ea884bfc2752dce5858fd6a5194e596ba3856557
alannarisse <angrypirate@gmail.com>
2019-05-01 00:25:05 -0700

    fixing docs archive page, and moved docs cards to include file

--- wp-content/themes/plusplus2019/archive-docs.php
+++ wp-content/themes/plusplus2019/archive-docs.php
@@ -2,42 +2,11 @@
 
 <main>
 	<div class="top-horiz">
-			<h3>Featured Documents</h3>
+			<h3>Documentation</h3>
 			<?php get_template_part('images/inline', 'bkg_horiz.svg'); ?>
 		</div>
-		<section>
-			
+		
 
-			<?php 
-			$args = array('post_type' => 'docs',
-				'post_per_page' => 20,
-				'post__in' => get_option( 'sticky_posts' ),
-				'ignore_sticky_posts' => 2
-			);
-			$sticky_loop = new WP_Query( $args );
-
-			if($sticky_loop->have_posts()) : ?>
-			<?php while($sticky_loop->have_posts()) : $sticky_loop->the_post(); ?>
-				<?php get_template_part('content'); ?>
-			<?php endwhile; ?>
-			<?php else : ?>
-				<?php echo wpautop('Sorry, no posts were found'); ?>
-			<?php endif; ?>
-		</section>
-<div class="mid-horiz"><?php get_template_part('images/inline', 'bkg_horiz.svg'); ?></div>
-
-		<section>
-			<?php 
-			$args = array('post_type' => 'docs', 'post_per_page' => 20);
-			$loop = new WP_Query( $args );
-
-			if($loop->have_posts()) : ?>
-			<?php while($loop->have_posts()) : $loop->the_post(); ?>
-				<?php get_template_part('content'); ?>
-			<?php endwhile; ?>
-			<?php else : ?>
-				<?php echo wpautop('Sorry, no posts were found'); ?>
-			<?php endif; ?>
-		</section>
+		<?php get_template_part('content-docs-cards'); ?>
 	</main>
 	<?php get_footer(); ?>
--- wp-content/themes/plusplus2019/page-docs-template.php
+++ wp-content/themes/plusplus2019/page-docs-template.php
@@ -15,27 +15,8 @@ Template Name: Docs Template
 			</div>
 			<div><?php get_template_part('images/inline', 'bkg-pluses_lg_grey.svg'); ?></div>
 		</section>
-		<section class="no-grid">
-			<?php 
-			$args = array('post_type' => 'docs', 'post_per_page' => 20);
-			$loop = new WP_Query( $args );
-
-			if($loop->have_posts()) : ?>
-				<ul class="docs-cards">
-			<?php while($loop->have_posts()) : $loop->the_post(); ?>
-				<?php if (get_post_status($status->ID) !== "private") : ?>
-					<li><h1><?php the_title(); ?></h1>
-					<!-- Category: <?php the_category( ', ' ); ?> -->
-					<?php /* $content = get_the_content(); echo mb_strimwidth($content, 0, 300, '...'); */?>
-					<?php  echo wp_trim_words( get_the_content(), 40, '...' );  ?>
-					<p><a class="btn-read-more" href="<?php the_permalink(); ?>" ><img src="/wp-content/themes/plusplus2019/images/icon-plusdouble.png"> Read more <img src="/wp-content/themes/plusplus2019/images/icon-plusdouble.png"></a></p>
-					</li>
-				<?php endif; ?>
-			<?php endwhile; ?>
- 				</ul>
-			<?php else : ?>
-				<?php echo wpautop('Sorry, no posts were found'); ?>
-			<?php endif; ?>
-		</section>
+	
+		
+		<?php get_template_part('content-docs-cards'); ?>
 	</main>
 <?php get_footer(); ?>
