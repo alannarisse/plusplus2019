2461a9bea30c0e813fa190f609271d08bcdd9239
alannarisse <angrypirate@gmail.com>
2019-06-10 20:42:51 -0700

    removed extra column for aside

--- wp-content/themes/plusplus2019/single-post.php
+++ wp-content/themes/plusplus2019/single-post.php
@@ -5,33 +5,34 @@
 			<?php if(have_posts()) : ?>
 			<?php while(have_posts()) : the_post(); ?>
 				<?php get_template_part('content'); ?>
-				<aside>&nbsp;</aside>
+				<!-- Aside -->
+				<aside>
+					<?php 
+					$args = array('post_type' => 'release', 'post_per_page' => 30);
+					$loop = new WP_Query( $args );
+					
+					if($loop->have_posts()) : ?>
+						<h4>More Documents</h4>
+						<ul class="docs-aside">
+							<?php while($loop->have_posts()) : $loop->the_post(); ?>
+								<?php if (get_post_status($status->ID) !== "private") : ?>
+									<li><h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+									<?php  echo wp_trim_words( get_the_content(), 20, '...' );  ?>
+									</li>
+								<?php endif; ?>
+							<?php endwhile; ?>
+		 				</ul>
+					<?php else : ?>
+						<?php echo wpautop('Sorry, no posts were found'); ?>
+					<?php endif; ?>
+				</aside>
+				<!-- End Aside -->
+
 		<?php endwhile; ?>
 		<?php else : ?>
 			<?php echo wpautop('Sorry, no posts were found'); ?>
 		<?php endif; ?>
 			
-
-			<aside>
-			<?php 
-			$args = array('post_type' => 'post', 'post_per_page' => 30);
-			$loop = new WP_Query( $args );
-			
-			if($loop->have_posts()) : ?>
-				<h4>More Articles</h4>
-				<ul class="docs-aside">
-					<?php while($loop->have_posts()) : $loop->the_post(); ?>
-						<?php if (get_post_status($status->ID) !== "private") : ?>
-							<li><h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-							<?php  echo wp_trim_words( get_the_content(), 20, '...' );  ?>
-							</li>
-						<?php endif; ?>
-					<?php endwhile; ?>
- 				</ul>
-			<?php else : ?>
-				<?php echo wpautop('Sorry, no posts were found'); ?>
-			<?php endif; ?>
-		</aside>
 			</section>
 		</main>
 
