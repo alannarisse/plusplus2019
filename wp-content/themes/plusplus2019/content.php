c5bf85ed799c6b0aaf70780ed3cdda3d9f5e1410
alannarisse <angrypirate@gmail.com>
2019-06-11 22:34:56 -0700

    linkable title

--- wp-content/themes/plusplus2019/content.php
+++ wp-content/themes/plusplus2019/content.php
@@ -3,10 +3,9 @@
 		<?php if(has_post_thumbnail()) : ?>
 			<div class="post-thumbnail"><a href="<?php the_permalink(); ?>" ><?php the_post_thumbnail(); ?></a></div>
 		<?php endif; ?>
-		<h2><?php the_title(); ?></h2>
+		<h2><a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></h2>
 		<?php /* the_category( ', ' );  */?>
 		<p><?php the_content(); ?></p>
-		
 		<p>
 			Category: <?php the_category( ', ' ); ?> Author: <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> Date: <?php the_time('F j, Y g:i a'); ?></p>
 	</article>
