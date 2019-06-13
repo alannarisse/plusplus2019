04050a00258083507e9b6fc2a7b04662806d74f6
Marko Gargenta <marko@plusplus.co>
2019-06-12 17:35:16 -0700

    Fixes large cover images on mobile

--- wp-content/themes/plusplus2019/page-products.php
+++ wp-content/themes/plusplus2019/page-products.php
@@ -3,21 +3,21 @@
 	<main>
 		<section>
 		<div>
-			<?php 
+			<?php
 			if (have_posts()) {
 				while (have_posts()) {
 					the_post();
-					the_content(); 
+					the_content();
 				}
 			} ?>
-<!-- 
+<!--
 			<h1>Enterprise knowledge sharing platform</h1>
 			<h4>Your organization's fast growth depends on your people. And the PlusPlus talent development platform delivers--at scale--what they need: live learning, mentoring, and coaching.</h4> -->
 		</div>
 		<div>
 
-			<img src="/wp-content/themes/plusplus2019/images/bkg_table_lav.png">
-			
+			<img class="fit-cover" src="/wp-content/themes/plusplus2019/images/bkg_table_lav.png">
+
 		</div>
 
 
@@ -38,11 +38,11 @@
 
 			<p>Those new to your organization or seeking to learn new skills can find upcoming events and enroll (or get added to a waitlist if a session is full). They can even take control of their career development by viewing the profile of a peer or a senior team member who they respect and finding classes relevant to their own needs. They’re also able to request that presenters repeat popular events or hold events in another location or online.</p>
 			<a href="/products/live-learning" class="button">Learn More</a>
-			
+
 		</div>
 		<!-- // live-learning -->
 
-		
+
 		<!-- Coaching  Mentoring -->
 		<div>&nbsp;</div>
 		<div class="horiz-right"><?php get_template_part('images/inline', 'bkg_horiz.svg'); ?>
@@ -61,8 +61,8 @@
 			<img src="/wp-content/themes/plusplus2019/images/products/Coaching-index.png" style="width: 100%;">
 		</div>
 		<!-- // Coaching  Mentoring -->
-		
-		
+
+
 		<div class="horiz-left"><?php get_template_part('images/inline', 'bkg_horiz.svg'); ?>
 		</div>
 		<div>&nbsp;</div>
@@ -79,11 +79,11 @@
 			<p>With PlusPlus, your people get more deep focus time while we handle the routine--from virtual introductions between mentors and mentees, coaches and coachees to calendar invites, reminders, and waitlist management.
 			</p>
 			<a href="/products/platform" class="button">Learn More</a>
-			
+
 		</div>
 
 
-		
+
 	</section>
 	</main>
 	<?php get_footer(); ?>
